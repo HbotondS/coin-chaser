c0e074bc2ef0bd0b5a51bb16a97821d9f574dd4e
HbotondS <hbotond97@gmail.com>
2023-01-04 23:05:29 +0200

    test out fxgl

--- src/main/java/com/HbotondS/coin_chaser/Main.java
+++ src/main/java/com/HbotondS/coin_chaser/Main.java
@@ -1,7 +1,15 @@
 package com.HbotondS.coin_chaser;
 
-public class Main {
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+public class Main extends GameApplication {
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setWidth(200);
+        settings.setHeight(200);
+    }
     public static void main(String[] args) {
-        System.out.println("hello");
+        launch(args);
     }
 }
