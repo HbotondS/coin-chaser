f596b16cec18b9b99a539c2627ae4f98aaec6d91
HbotondS <hbotond97@gmail.com>
2023-01-20 13:17:25 +0200

    fix saving error in kotlin

--- src/main/kotlin/com/hbotonds/coin_chaser/mongodb/gateway/HighScoreKt.kt
+++ src/main/kotlin/com/hbotonds/coin_chaser/mongodb/gateway/HighScoreKt.kt
@@ -3,13 +3,19 @@ package com.hbotonds.coin_chaser.mongodb.gateway
 import org.bson.codecs.pojo.annotations.BsonProperty
 import org.bson.types.ObjectId
 
-class HighScoreKt(id: ObjectId, name: String, score: Int) {
+class HighScoreKt {
     constructor() : this(ObjectId.get(), "", 0)
 
-    lateinit var id: ObjectId
+    constructor(id: ObjectId, name: String, score: Int) {
+        this.id = id
+        this.name = name
+        this.score = score
+    }
+
+    var id: ObjectId
 
     @BsonProperty(value = "name")
-    lateinit var name: String
+    var name: String
 
     @BsonProperty(value = "score")
     var score: Int = 0
